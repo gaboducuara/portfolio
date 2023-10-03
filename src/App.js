f33c1a37970a6b48290a1952e4a0b956d68abe69
Gabriel <gabrielmancilladucuara@gmail.com>
2023-10-03 09:14:13 -0600

    actualizando datos

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
                         <Route path="/" element={<Home />}></Route>
                         <Route exact path="/about" element={<About />}></Route>
                         {/* <Route exact path="/service" element={<Services />}></Route> */}
-                        {/* <Route exact path="/project" element={<Project />}></Route> */}
+                        <Route exact path="/project" element={<Project />}></Route>
                         {/* <Route exact path="/project/app" element={<ProjectApp />} />
                         <Route exact path="/project/game" element={<ProjectGame />} /> */}
                     </Routes>
